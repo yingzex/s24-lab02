dd5189547eaf6e75bb8c3ee7eef4ce113a6cb24f
xyz <15109268895x@gmail.com>
2024-01-24 11:06:32 -0500

    encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,10 +1,10 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
-    
-    public Rectangle(double height, double width){
+    private double height;
+    private double width;
+
+    public Rectangle(double height, double width) {
         this.height = height;
         this.width = width;
     }
